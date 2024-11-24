f6d8aeeea2e09233dcc4c9665f5eccda285d2f9c
Henrique Marlon <henriquemarloncs@gmail.com>
2024-11-24 01:15:08 -0300

    feat: contract impl

---
+++ contracts/src/Token.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.0.0
+pragma solidity ^0.8.22;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract Token is ERC20 {
+    constructor(string memory name, string memory symbol) ERC20(name, symbol) {}
+
+    function mint(address to, uint256 amount) public {
+        _mint(to, amount);
+    }
+}
