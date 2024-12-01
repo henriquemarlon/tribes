6621196cacdaa9b6c453f894d6a8d813895d6032
Henrique Marlon <henriquemarloncs@gmail.com>
2024-11-30 21:56:35 -0300

    Feature/cancel order (#4)
    * feat: cancel order logic
    
    * feat: update README
    
    * refactor: rbac middleware

--- internal/infra/cartesi/middleware/rbac.go
+++ internal/infra/cartesi/middleware/rbac.go
@@ -5,6 +5,8 @@ import (
 	"database/sql"
 	"errors"
 	"fmt"
+
+	"github.com/ethereum/go-ethereum/common"
 	"github.com/rollmelette/rollmelette"
 	"github.com/tribeshq/tribes/internal/domain/entity"
 	"github.com/tribeshq/tribes/internal/usecase/user_usecase"
@@ -23,53 +25,34 @@ func NewRBACMiddleware(userRepository entity.UserRepository) *RBACMiddleware {
 
 func (m *RBACMiddleware) Middleware(handlerFunc router.AdvanceHandlerFunc, roles []string) router.AdvanceHandlerFunc {
 	return func(env rollmelette.Env, metadata rollmelette.Metadata, deposit rollmelette.Deposit, payload []byte) error {
+		var address common.Address
+		ctx := context.Background()
 		erc20Deposit, ok := deposit.(*rollmelette.ERC20Deposit)
 		if ok {
-			ctx := context.Background()
-			findUserByAddress := user_usecase.NewFindUserByAddressUseCase(m.UserRepository)
-			user, err := findUserByAddress.Execute(ctx, &user_usecase.FindUserByAddressInputDTO{
-				Address: erc20Deposit.Sender,
-			})
-			if err != nil {
-				if errors.Is(err, sql.ErrNoRows) {
-					return fmt.Errorf("user not found during RBAC middleware check")
-				}
-				return err
-			}
-			var hasRole bool
-			for _, role := range roles {
-				if user.Role == role {
-					hasRole = true
-					break
-				}
-			}
-			if !hasRole {
-				return fmt.Errorf("user with address: %v don't have necessary permission", user.Address)
-			}
-			return handlerFunc(env, metadata, deposit, payload)
+			address = erc20Deposit.Sender
 		} else {
-			ctx := context.Background()
-			findUserByAddress := user_usecase.NewFindUserByAddressUseCase(m.UserRepository)
-			user, err := findUserByAddress.Execute(ctx, &user_usecase.FindUserByAddressInputDTO{
-				Address: metadata.MsgSender,
-			})
-			if err != nil {
-				if errors.Is(err, sql.ErrNoRows) {
-					return fmt.Errorf("user not found during RBAC middleware check")
-				}
-				return err
-			}
-			var hasRole bool
-			for _, role := range roles {
-				if user.Role == role {
-					hasRole = true
-					break
-				}
-			}
-			if !hasRole {
-				return fmt.Errorf("user with address: %v don't have necessary permission", user.Address)
-			}
-			return handlerFunc(env, metadata, deposit, payload)
+			address = metadata.MsgSender
 		}
+		findUserByAddress := user_usecase.NewFindUserByAddressUseCase(m.UserRepository)
+		user, err := findUserByAddress.Execute(ctx, &user_usecase.FindUserByAddressInputDTO{
+			Address: address,
+		})
+		if err != nil {
+			if errors.Is(err, sql.ErrNoRows) {
+				return fmt.Errorf("user not found during RBAC middleware check")
+			}
+			return err
+		}
+		var hasRole bool
+		for _, role := range roles {
+			if user.Role == role {
+				hasRole = true
+				break
+			}
+		}
+		if !hasRole {
+			return fmt.Errorf("user with address: %v does not have necessary permissions: %v", user.Address, roles)
+		}
+		return handlerFunc(env, metadata, deposit, payload)
 	}
 }
